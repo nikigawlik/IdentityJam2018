5215a264eec9c7182876289b9c27bc5794b73cb5
Nils Gawlik <nilsgawlik@gmx.de>
2018-12-01 19:30:17 +0100

    Implement maximum amount of pickupable objects

--- Assets/Scripts/CollectionOfStuff.cs
+++ Assets/Scripts/CollectionOfStuff.cs
@@ -6,6 +6,7 @@ public class CollectionOfStuff : MonoBehaviour {
 	public List<Collectable> collectedObjects;
 	public float minOrbitRadius = 2f;
 	public float orbitRadiusIncrease = 2f;
+	public int maxCollectables = 4;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,24 @@ public class CollectionOfStuff : MonoBehaviour {
 	}
 
 	public void AddCollectable(Collectable c) {
+		// drop collectibles
+		if(collectedObjects.Count >= maxCollectables) {
+			Collectable dropThis = collectedObjects[Random.Range(0, collectedObjects.Count)];
+			collectedObjects.Remove(dropThis);
+			
+			// remove orbit
+			Object.Destroy(dropThis.GetComponent<Orbit>());
+			MoveTo mt = dropThis.gameObject.AddComponent<MoveTo>();
+			mt.targetPosition = c.transform.position;
+			dropThis.transform.SetParent(null);
+
+			// reenable stuff
+			InteractableObject io = dropThis.gameObject.GetComponent<InteractableObject>();
+			io.enabled = true;
+			Collider col = io.GetComponent<Collider>();
+			col.enabled = true;
+		}
+
 		collectedObjects.Add(c);
 		Orbit orbit = c.gameObject.AddComponent<Orbit>();
 		orbit.target = transform;
---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+	public Vector3 targetPosition;
+	public float lerpFactor = 15f;
+	public float arriveRadius = .2f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, targetPosition, lerpFactor * Time.deltaTime);
+		if(Vector3.Distance(transform.position, targetPosition) < arriveRadius) {
+			Object.Destroy(this);
+		}
+	}
+}
