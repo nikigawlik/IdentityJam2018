d7fe9f0fb606f51de2f19cc44f3f71b619480c78
Nils Gawlik <nilsgawlik@gmx.de>
2018-12-01 16:06:16 +0100

    Add basic interactable object logic

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+	public float highlightScale = 1.5f;
+	public float highlightDecay = 5f;
+	public float highlightBuildup = 15f;
+	public Transform display;
+
+	private float scale;
+
+	private void OnTriggerStay(Collider other) {
+		PlayerController pc = other.GetComponent<PlayerController>();
+		if(pc) {
+			display.localScale = Vector3.Lerp(display.localScale, Vector3.one * highlightScale, highlightBuildup * Time.fixedDeltaTime);
+		}
+	}
+
+	private void FixedUpdate() {
+		display.localScale = Vector3.Lerp(display.localScale, Vector3.one, highlightDecay * Time.fixedDeltaTime);
+	}
+}
